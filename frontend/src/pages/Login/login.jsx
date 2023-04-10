83695444d50c46f77eba91fb60a1b48bd5ba4ccd
Dufi1928 <i.hrybe@gmail.com>
2023-04-10 16:09:13 +0200

    feat(login): create visual layout for login page
    - Implement login page layout with form and image
    - Add styles for login page components

--- frontend/src/pages/Login/login.jsx
+++ frontend/src/pages/Login/login.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "../../scss/App.scss";
+import "./login.scss";
+import { IoLockClosedOutline } from "react-icons/io5";
+import { HiOutlineMail } from "react-icons/hi";
+
+
 import Header from "../../components/Header/Header";
 import { useNavigate } from "react-router-dom";
+import Futter from "../../components/Futter/Futter";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -29,35 +35,65 @@ function Login() {
   return (
     <div className="App">
       <Header />
-      <div className="login-container">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>Email:</label>
-            <input
-              type="email"
-              className="form-control"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+      <div className="login-container container">
+        <div className="row loginPageContent ">
+          <div className="col-7 leftSideLogin">
+              <div className="login-img">
+                <img
+                  src="/img/login/Astronaut.svg"
+                  alt="login-img"
+                />
+              </div>
+                <div className="login_title">
+                    <h3>Bienvenue à bord !</h3>
+                    <p>Juste quelques clics avant de commencer !</p>
+                </div>
+
+              
           </div>
-          <div className="form-group">
-            <label>Password:</label>
-            <input
-              type="password"
-              className="form-control"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+          <div className="col-5 rightSide">
+            <h3>Bonjour</h3>
+            <form onSubmit={handleSubmit}>
+              <div className="my-form" >
+                <div className=" input-icon-container">
+                  <HiOutlineMail className="input-icon" />
+                  <input
+                    type="email"
+                    className="input-with-icon"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="">
+                <div className=" input-icon-container">
+                  <IoLockClosedOutline className="input-icon" />
+                  <input
+                    type="password"
+                    className=" input-with-icon"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="forgetButton">
+                <a href="#"> Mot de passe oublié ? </a>
+              </div>
+              {error && <div className="alert alert-danger">{error}</div>}
+              <div className="submit-button">
+                  <button type="submit">
+                    Connexion
+                  </button>
+              </div>
+            </form>
           </div>
-          {error && <div className="alert alert-danger">{error}</div>}
-          <button type="submit" className="btn btn-primary">
-            Login
-          </button>
-        </form>
+        </div>
       </div>
+      <Futter />
     </div>
   );
 }
